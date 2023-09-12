7b417c842482be0f6b50370f514ba50160096104
Antismart <timbwamoses83@gmail.com>
2023-09-12 12:26:57 +0300

    improve accessibility

--- lib/main.dart
+++ lib/main.dart
@@ -34,11 +34,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
     return Scaffold(
       body: Column(
         children: [
           Text('A Random Idea:'),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair),
           ElevatedButton(
             onPressed: () {
               print('Button Pressed!');
@@ -50,6 +51,34 @@ class MyHomePage extends StatelessWidget {
     );
   }
   }
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase, 
+          style: style, 
+          semanticsLabel: "${pair.first} ${pair.second}"
+        ),
+      ),
+    );
+  }
+}
 
   
   
